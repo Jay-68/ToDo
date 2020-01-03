8e963f83fa6078b40ff29a84283e584e5b6b4bc7
Jay-68 <kelvinngari1995@gmail.com>
2020-01-03 19:13:45 +0300

    change boolean field in the models and make migrations

--- my_todo/models.py
+++ my_todo/models.py
@@ -5,7 +5,7 @@ from django.db import models
 
 class Todo(models.Model):
     text = models.CharField(max_length=30)
-    completed = models.BooleanField(default=False)
+    complete = models.BooleanField(default=False)
 
     def __str__(self):
         return self.text
